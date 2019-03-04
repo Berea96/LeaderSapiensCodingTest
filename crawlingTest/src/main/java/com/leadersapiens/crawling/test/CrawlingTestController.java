da0e0762d8029e9dba05c2abaa3a4520bd489152
mac <kim-kj960712@daum.net>
2019-03-04 10:40:34 +0900

    오타 수정

--- crawlingTest/src/main/java/com/leadersapiens/crawling/test/CrawlingTestController.java
+++ crawlingTest/src/main/java/com/leadersapiens/crawling/test/CrawlingTestController.java
@@ -1,12 +1,14 @@
 package com.leadersapiens.crawling.test;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 //크롤링을 실시하는 메인 컨트롤러라고 할수있다.
 @RestController
-@RequestMapping(value = "/crawlingTest")
+@RequestMapping(value = "/football")
 public class CrawlingTestController {
 
     @Autowired
@@ -20,7 +22,7 @@ public class CrawlingTestController {
     }
 
     //본격적으로 축구리그 이름에 따라서 Json을 얻어오는 메소드
-    @RequestMapping(value = "/getFootball", mehtod = RequestMethod.GET)
+    @RequestMapping(value = "/getFootball", method = RequestMethod.GET)
     public String crawlingFootball() {
 
 
@@ -34,7 +36,7 @@ public class CrawlingTestController {
     }
 
     //테스트 url로 매핑한 메소드
-    @ReuqestMapping(value = "/football/rank/{leagueName}", RequestMethod.GET)
+    @RequestMapping(value = "/rank/{leagueName}", method = RequestMethod.GET)
     public String crawlingFootball(@PathVariable String leagueName) {
         String rank = service.crawlingFootballData(leagueName);
 
